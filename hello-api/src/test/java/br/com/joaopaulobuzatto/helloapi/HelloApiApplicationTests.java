ae4b8d7939f404beb42f580860c9a0f4768b10cd
Joao Paulo Buzatto <joaopaulobuzatto@gmail.com>
2024-04-01 22:16:09 -0300

    first commit

---
+++ hello-api/src/test/java/br/com/joaopaulobuzatto/helloapi/HelloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaopaulobuzatto.helloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ola-api/src/test/java/br/com/joaopaulobuzatto/olaapi/OlaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaopaulobuzatto.olaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
